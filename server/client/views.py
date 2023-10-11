d64d8624f590a39ef69002c585c243a5b57fa55b
gharbi-collab <mohamedhedigharbi101@gmail.com>
2023-10-11 21:00:41 +0100

    module

---
+++ server/client/views.py
@@ -0,0 +1,49 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import AccountSerializer
+from rest_framework import status 
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+@api_view(['POST'])
+def login(request):
+    user = get_object_or_404(User, username = request.data['username'])
+
+    if not user.check_password(request.data['password']):
+        return Response({"detail": "Not found."}, status = status.HTTP_404_NOT_FOUND)
+    
+    token, created = Token.objects.get_or_create(user = user)
+    serializer = AccountSerializer(instance = user)
+
+    return Response({"token": token.key, "user": serializer.data})
+
+
+@api_view(['POST'])
+def signup(request):
+    serializer = AccountSerializer(data = request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username = request.data['username'])
+        user.set_password(request.data['password'])
+        user.save()
+        token = Token.objects.create( user = user)
+        return Response({"token": token.key, "user": serializer.data})
+    
+    return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def test_token(request):
+    return Response("passed for {}".format(request))
+
+
+
