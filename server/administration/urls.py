bc99e3b5ae3e229b4d65c225afb5e8c296b324ad
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-10-11 15:37:00 +0100

    add verification code endpoints

--- server/administration/urls.py
+++ server/administration/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('login/', views.login)
+  path('login/', views.login),
+  path('code-verification/', views.verify_code),
+  path('check-code-verification/', views.check_verify_code)
 ]
