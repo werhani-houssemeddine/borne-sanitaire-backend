c363c3b4e3aeec1f5e4a4cc0c7ab2658007358e6
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-10-15 21:30:11 +0100

    add email configuration

--- server/server/settings.py
+++ server/server/settings.py
@@ -127,3 +127,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CALLING .ENV TO GET ENVIRONMENT VARIABLES
+env = environ.Env()
+environ.Env.read_env()
+
+# CONFIG THE SMTPL GMAIL SERVER CONFIGURATION
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
