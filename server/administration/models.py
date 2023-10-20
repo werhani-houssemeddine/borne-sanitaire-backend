e118ffe5fc6f10904a60d3821ef57c2359b9da75
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-10-20 21:27:08 +0100

    add session_id to verification_code table

--- server/administration/models.py
+++ server/administration/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 class VerificationCode(models.Model):
   code = models.CharField(max_length=12, null=False)
@@ -9,3 +10,4 @@ class VerificationCode(models.Model):
   )
   created_at = models.DateTimeField(auto_now_add=True)
   number_of_attempts = models.IntegerField(default=0)
+  session_id = models.UUIDField(default = uuid.uuid4, editable = False, unique = True)
