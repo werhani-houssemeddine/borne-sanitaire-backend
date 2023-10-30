85d56b18bd4ef64636555ee4a25971bd56f90ad4
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-10-30 17:06:04 +0100

    bcrypt to compare password and hashed password

---
+++ server/lib/bcrypt.py
@@ -0,0 +1,41 @@
+import bcrypt
+
+
+#! Bcrypt class is to hash and compare values
+#! calling hash to hash instannce and compare to 
+#! compare instances
+
+class Bcrypt:
+  rounds: int = 12
+
+  # In other language we may make it private and will
+  # be called from hash and compare functions
+  @staticmethod
+  def generateSalt(rounds: int = 12) -> bytes:
+    print(rounds)
+    return bcrypt.gensalt(rounds)
+
+
+  @staticmethod
+  def compare(instance1: str, instance2: str) -> bool:
+    if isinstance(instance1, str):
+      instance1 = instance1.encode('utf-8')
+
+    if isinstance(instance2, str):
+      instance1 = instance2.encode('utf-8')
+
+    return bcrypt.checkpw(
+      hashed_password = instance2,
+      password        = instance1
+    )
+  
+
+  @staticmethod
+  def hash(instance: bytes) -> bytes:
+    if isinstance(instance, str):
+      instance = instance.encode('utf-8')
+
+    return bcrypt.hashpw(
+      password = instance,
+      salt     = Bcrypt.generateSalt(Bcrypt.rounds) 
+    )
