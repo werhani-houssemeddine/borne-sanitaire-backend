9625dfde7dd759f9e31cf80e03baf870f16150eb
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-01 16:09:16 +0100

    split cookies

--- server/lib/Http/headers.py
+++ server/lib/Http/headers.py
@@ -3,7 +3,7 @@ class RequestHeaders:
 
     self.contentLength = headers.get('Content-Length')
     self.contentType   = headers.get('Content-Type')
-    self.cookies       = headers.get('Cookie')
+    self.cookies       = headers.get('Cookie').split('; ')
     self.agent         = headers.get('User-Agent')
     self.accept        = headers.get('Accept').split(',')
     self.host          = headers.get('Host')
