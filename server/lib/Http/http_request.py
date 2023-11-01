ea5c0e99c8b90590d2218277ee3d19076c1d1417
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-01 11:50:51 +0100

    add query

--- server/lib/Http/http_request.py
+++ server/lib/Http/http_request.py
@@ -1,10 +1,11 @@
 # This class will create a HTTP_REQUEST we will work 
 # with this request object instead of the default http.Request 
 class HTTP_REQUEST:
-  def __init__(self, ip_address, headers, method, params, body, path, url):
+  def __init__(self, ip_address, headers, method, params, body, path, url, query):
     self.headers = headers
     self.method  = method
     self.params  = params
+    self.query   = query 
     self.body    = body
     self.path    = path
     self.url     = url
