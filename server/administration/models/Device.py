e79de7f069e237cab692079d6fba797613283a9d
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-01 13:06:52 +0100

    import User model and add _ondelete property to user_id

--- server/administration/models/Device.py
+++ server/administration/models/Device.py
@@ -1,9 +1,11 @@
 from django.db import models
 import uuid
 
-class VerificationCode(models.Model):
+from client.models import User
+
+class Device(models.Model):
   device_id = models.UUIDField(default = uuid.uuid4, editable = False, unique = True)
-  user_id = models.ForeignKey('USER', default=None)
+  user_id = models.ForeignKey(User, default=None, on_delete= models.SET_NULL, null=True)
   version = models.CharField(max_length=8, null=False)
   is_saled = models.BooleanField(default=False)
   created_at = models.DateTimeField(auto_now_add=True)
