93811bc7cb7dd96195d0e30109e6392fb337bcc2
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-01 13:21:20 +0100

    verification code middleware

---
+++ server/administration/authentication/Middleware/code.py
@@ -0,0 +1,62 @@
+from lib.Http     import HTTP_REQUEST, RESPONSE_SAMPLE
+from Mail         import sendHTMLContentEmail
+from Mail.samples import HTMLSample
+
+from administration.authentication.VerificationCode import VerificationCodeController
+from administration.authentication.Login            import AuthenticationController
+
+def VerificationCodeMiddleware(request: HTTP_REQUEST):
+  try:
+    verificationCodeResponse =VerificationCodeController.handleCodeComparision(
+      ip   = request.ip,
+      code = request.body.get('verification_code')
+    )
+
+    if verificationCodeResponse == 'success':
+      return {
+        'status_code': 200,
+        'body'       : {
+          'message' : 'Verification Code is correct',
+          'state'   : 'success',
+          'error'   : False,
+          'data'    : { 
+            'is_blocked': False,
+            'token'     : 'token will be available soon ☺️',
+          }
+        }
+      }
+
+    #! must add the logic of blocking users
+    elif verificationCodeResponse == 'failed':
+      return {
+        'status_code': 200,
+        'body'       : {
+          'message': 'Verification Code is not correct',
+          'state'  : 'failure',
+          'error'  : False,
+          'data'   : {
+            'is_blocked': False
+          }
+        }
+      }
+    
+    #? User is Blocked
+    else:
+      return {
+        'status_code': 403,
+        'body'       : {
+          'message': 'Blocked for providing wrong verification code more than 3 times',
+          'state'  : 'failure',
+          'error'  : True,
+          'data'   : {
+            'is_blocked': True
+          }
+        }
+      }
+    
+  except Exception as e:
+    return RESPONSE_SAMPLE.badRequest({ 'details': str(e) })
+  
+
+def CheckVerificationCodeMiddleware(request: HTTP_REQUEST):
+  pass
