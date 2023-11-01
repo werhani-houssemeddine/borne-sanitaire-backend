d707457f80885452416b92755358490c28a41ae8
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-01 15:33:56 +0100

    remove unnecessary code

--- server/client/views.py
+++ server/client/views.py
@@ -1,23 +1,11 @@
-from rest_framework.response import Response
+from rest_framework.response   import Response
 from rest_framework.decorators import api_view
-from .serializers import AccountSerializer
-from rest_framework import status 
-from rest_framework.authtoken.models import Token
-from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404
-
-from rest_framework.decorators import authentication_classes, permission_classes
-from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-
 
 from client.authentication.Signup import SignUpController
 from client.authentication.Login  import LoginController
 from lib.make_request             import makeRequest
 from lib.token                    import Token  
 
-
-# from ..lib import makeRequest
 
 @api_view(['GET'])
 def currentUser(request):
@@ -53,13 +41,3 @@ def login(request):
 def signup(request):
     response = makeRequest(request = request, middleware = SignUpController.singup)
     return Response(status = response.status_code, data = response.body)
-
-
-@api_view(['GET'])
-@authentication_classes([SessionAuthentication, TokenAuthentication])
-@permission_classes([IsAuthenticated])
-def test_token(request):
-    return Response("passed for {}".format(request))
-
-
-
