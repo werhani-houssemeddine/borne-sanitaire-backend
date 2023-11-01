0e5d15548e7e7f0ec1989517a688a21ad2a0a386
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-01 15:52:38 +0100

    Header class

---
+++ server/lib/Http/headers.py
@@ -0,0 +1,10 @@
+class RequestHeaders:
+  def __init__(self, headers: dict) -> None:
+
+    self.contentLength = headers.get('Content-Length')
+    self.contentType   = headers.get('Content-Type')
+    self.cookies       = headers.get('Cookie')
+    self.agent         = headers.get('User-Agent')
+    self.accept        = headers.get('Accept').split(',')
+    self.host          = headers.get('Host')
+    
