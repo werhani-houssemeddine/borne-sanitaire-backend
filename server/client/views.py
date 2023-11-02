ee5707eb6af5686298171adf91fdf7eb069aac36
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-03 00:24:23 +0100

    to much refactoring remove current user and change some importation files

--- server/client/views.py
+++ server/client/views.py
@@ -1,43 +1,17 @@
 from rest_framework.response   import Response
 from rest_framework.decorators import api_view
 
-from client.authentication.Signup import SignUpController
-from client.authentication.Login  import LoginController
-from lib.make_request             import makeRequest
-from lib.token                    import Token  
-
-
-@api_view(['GET'])
-def currentUser(request):
-    token = request.headers['Authorization']
-    if(token == None):
-        return Response({ 'token': 'Not Found' }, status = 401)
-    else:
-        try:
-            
-
-            payload = {
-                "email": "houssemwuerhani@gmail.com",
-                "password": "123456789",
-                "id": 1
-            }
-
-            jwt_token = Token.createToken(payload)
-            
-            return Response({ 'token': jwt_token })
-        
-        except Exception as e:
-            print(f"Exception {e}")
-            return Response({ 'token': None }, status = 401)
-        
+from client.authentication.Middleware import LoginMiddleware
+from client.authentication.Middleware import SignupAdminMiddleware
+from lib.make_request                 import makeRequest
 
 @api_view(['POST'])
 def login(request):
-    response = makeRequest(request = request, middleware = LoginController.login)
+    response = makeRequest(request = request, middleware = LoginMiddleware)
     return Response(status = response.status_code, data = response.body)
 
 
 @api_view(['POST'])
 def signup(request):
-    response = makeRequest(request = request, middleware = SignUpController.singup)
+    response = makeRequest(request = request, middleware = SignupAdminMiddleware)
     return Response(status = response.status_code, data = response.body)
