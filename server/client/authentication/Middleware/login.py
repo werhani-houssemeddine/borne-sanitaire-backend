d10c65e384e98139e3940208d71df02760a2467f
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-05 10:15:57 +0100

    add login Middleware

--- server/client/authentication/Middleware/login.py
+++ server/client/authentication/Middleware/login.py
@@ -1,4 +1,18 @@
 from lib.Http     import HTTP_REQUEST, RESPONSE_SAMPLE
+from client.authentication.Login import LoginController
 
 def LoginMiddleware(request: HTTP_REQUEST):
-  pass
+  try:
+    (email, password) = LoginController.chekUserData(request)
+    user = LoginController.checkCredentialse(email, password)
+
+    if user == None: 
+      return RESPONSE_SAMPLE.notAuthorised()
+
+    token    = LoginController.generateToken(user)
+    response = LoginController.generateResponse(user, token)
+
+    return RESPONSE_SAMPLE.ok(response)
+
+  except Exception as e:
+    return RESPONSE_SAMPLE.badRequest({ 'details': str(e) })
