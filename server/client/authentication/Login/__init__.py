1fe125762e268077cc994ddd3282b3c7cead91f8
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-05 10:15:35 +0100

    create a small functions instead of a big one

--- server/client/authentication/Login/__init__.py
+++ server/client/authentication/Login/__init__.py
@@ -1,64 +1,50 @@
 from lib.Http.http_request import HTTP_REQUEST
-from lib.Http              import RESPONSE_SAMPLE
 from lib.bcrypt            import Bcrypt 
 from lib.token             import Token
 
 from client.authentication.Login.business_rules import LogIn as LoginValidate
-from client.authentication.Repository           import User
-
+from client.Repository import User
+from client.models     import User as UserTable 
 class LoginController:
   
   @staticmethod
-  def login(request: HTTP_REQUEST):
+  def chekUserData(request: HTTP_REQUEST) -> tuple:
     try:
       # this function either return a tuple (email, password)
       # or throw a new error
       (email, password) = LoginValidate.validateUserData(request.body)
-
-      # Get User by looking user_email
+      return (email, password)
+    
+    except Exception as e:
+      raise e     
+    
+  @staticmethod
+  def checkCredentialse(email, password) -> UserTable | None:
+    try:
+      # Return User by looking user_email
       user = User.getUserByEmail(email)
 
-      # Check user existing
-      if user == None:
-        return wrongCredentials()
-      
       # Specify number of rounds for bcrypt hash method
-      # Comparing password
       Bcrypt.rounds = 10
+
+      # Comparing password
       if Bcrypt.compare(password, user.password) == False:
-        return wrongCredentials()
-        
-      # generate Token
-      payload = { 'email': email, 'id': user.id, 'username': user.user_name }
-      jwt_token = Token.createToken(payload)
-
-      return writeCredentials(token = jwt_token)
-
+        return None
+      return user
+      
     except Exception as e:
-      return RESPONSE_SAMPLE.badRequest({ 'details': str(e) })
+      raise e
   
-
-
-
-def wrongCredentials():
-  return {
-    'status_code': 401,
-      'body'       : {
-        'message': 'WRONG CREDENTIALS',
-        'state'  : 'failure',
-        'error'  : False,
-      }
-  }
-
-def writeCredentials(token: str) -> dict:
-  return {
-    'status_code': 200,
-      'body'       : {
-        'message': 'LOGIN SUCCESSFULLY',
-        'state'  : 'success',
-        'error'  : False,
-        'data': {
-          'token': token
-        }
-      }
-  }
+  @staticmethod
+  def generateToken(user: UserTable):
+    payload = { 'email': user.email, 'id': user.id, 'username': user.user_name, 'role': user.role }
+    return Token.createToken(payload)
+  
+  @staticmethod 
+  def generateResponse(user: UserTable, token: str) -> dict:
+    return {
+      'username': user.user_name,
+      'isAdmin' : user.role == "ADMIN",
+      'isAgent' : user.role == "AGENT",
+      'token'   : token
+    }
