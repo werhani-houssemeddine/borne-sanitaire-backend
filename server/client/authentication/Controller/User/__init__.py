c6a6953092e8cc07761aff25d1321edea25af179
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-10 16:37:20 +0100

    current user controller and middleware

--- server/client/authentication/Controller/User/__init__.py
+++ server/client/authentication/Controller/User/__init__.py
@@ -1,2 +1,29 @@
+from lib.Http.headers import RequestHeaders
+from lib.Http         import HTTP_REQUEST
+from lib.token        import Token
+
+def getToken(headers: RequestHeaders) -> str:
+  return headers.Authorization
+
+#? This controller will just return the user info
+#? the controller doesn't need to validate the token
+#? because the token will already checked with the decorator @Authenticate
+#? for better inforamtion just check lib.utils.auth
+#? the data comming to this controller is valid also we are sure 
+#? that the request have headers property which is also have Authorization property
+
+#! For future version we may add a singelton instance and pass it in @Authenticate
+
 class CurrentUserController:
-  pass
+  def getUser(request: HTTP_REQUEST):
+    token = getToken(request.headers)
+    payload_token = Token.getTokenPayload(token)
+    
+    return {
+      'id': payload_token['id'],
+      'user': {
+        'username': payload_token['username'],
+        'email'   : payload_token['email'],
+        'role'    : payload_token['role']
+      }  
+    }
--- server/client/authentication/Middleware/currentUser.py
+++ server/client/authentication/Middleware/currentUser.py
@@ -3,7 +3,13 @@ from client.authentication.Controller.User import CurrentUserController
 
 def CurrentUserMiddleware(request: HTTP_REQUEST):
   try:
-    print(request.headers)
+    #
+      # No need to check if the user == None because 
+      # we are too sure that the response is valid
+      # check the controller for more information
+    #
 
+    return RESPONSE_SAMPLE.ok(CurrentUserController.getUser(request))
+    
   except Exception as e:
     return RESPONSE_SAMPLE.badRequest({ 'details': str(e) })
