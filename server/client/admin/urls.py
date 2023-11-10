0a036d8347729e6c83b8580571712b4b5f07f3a4
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-10 16:07:02 +0100

    remove current-user from urls

--- server/client/admin/urls.py
+++ server/client/admin/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
-  path('current-user/', views.currentUser),
   path('edit/', views.edit),
   path('add-agent/', views.addAgent)
 ]
