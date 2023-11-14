550cc53b10ffb4070d268c61a050b5bf52540db6
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-14 22:45:31 +0100

    return a new format instead of boolean

--- server/administration/authentication/Login/controller.py
+++ server/administration/authentication/Login/controller.py
@@ -1,5 +1,6 @@
 from lib.Http.http_request import HTTP_REQUEST
 from .business_logic    import LoginBusinessLayer
+from lib.token import Token
     
 class AuthenticationController:
   @staticmethod 
@@ -11,8 +12,17 @@ class AuthenticationController:
         password = data.get('password'),
         email    = data.get('email')
       )
+      checkingResult = LoginBusinessLayer.check_credentials(email, password)
+      
+      if checkingResult != None:
+        user = checkingResult
+        payload = { 'email': user.email, 'username': user.username, 'phone_number': user.phone_number }
+        payload['token'] = Token.createToken(payload)
 
-      return LoginBusinessLayer.check_credentials(email, password)
+        return payload
+      
+      return None
+        
 
     except Exception as e:
       raise
