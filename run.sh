2ca39d5deca9d91fdd4fb524962cb3210115f8a9
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-19 07:40:23 +0100

    add comments and  a new command to run the server

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 
 # This file will load the necessary data for our application
 
+# Run python environment 
+source .env/Scripts/activate
 
-
+# Add the administrator actors if not exist 
 count=$(python server/manage.py shell -c "from administration.models import SuperAdmin; print(SuperAdmin.objects.count())")
 
 if [ "$count" -eq 0 ]; then
@@ -15,3 +17,6 @@ if [ "$count" -eq 0 ]; then
 else
   echo "The database is already not empty";
 fi
+
+# Run the server 
+python server/manage.py runserver
