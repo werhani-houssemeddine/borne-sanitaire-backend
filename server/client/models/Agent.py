f27ee43e6478df0d7536e492f89dcfab14514b69
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-24 21:47:58 +0100

    add agent_id to Agen table

--- server/client/models/Agent.py
+++ server/client/models/Agent.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 class Agent(models.Model):
-  
+
   id = models.AutoField( primary_key = True )
   susspend = models.BooleanField(default = False)
-  user_id = models.ForeignKey('User' , on_delete= models.CASCADE, default = None, null=True)
   
+  #? User Id will reference to the admin
+  user_id = models.ForeignKey('User' , on_delete= models.CASCADE, related_name = "agent_user_id")
+
+  #? Agent Id will reference to the agent from user table 
+  agent_id = models.ForeignKey('User', on_delete= models.CASCADE, related_name = "agent_agent_id")
 
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True) 
