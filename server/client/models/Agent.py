dbca3980ccde0636f99dfc0b283db9f3e5087465
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-24 22:16:29 +0100

    rename field

--- server/client/models/Agent.py
+++ server/client/models/Agent.py
@@ -3,7 +3,7 @@ from django.db import models
 class Agent(models.Model):
 
   id = models.AutoField( primary_key = True )
-  susspend = models.BooleanField(default = False)
+  suspend = models.BooleanField(default = False)
   
   #? User Id will reference to the admin
   user_id = models.ForeignKey('User' , on_delete= models.CASCADE, related_name = "agent_user_id")
