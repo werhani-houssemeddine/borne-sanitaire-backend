410ef741dce4c74fa183b7ae582ad7ec93e00c24
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-25 14:22:05 +0100

    login middleware

--- server/client/authentication/Middleware/login_middleware.py
+++ server/client/authentication/Middleware/login_middleware.py
@@ -1,23 +1,34 @@
-from lib.HTTP     import HTTP_REQUEST, HTTP_RESPONSE
+from lib.HTTP     import HTTP_REQUEST, HTTP_RESPONSE, RESPONSE_SAMPLE
+from lib.errors   import ValidationError
 
-from client.authentication.Controller.Login import LoginController
-
-# def LoginMiddleware(request: HTTP_REQUEST):
-#   try:
-#     (email, password) = LoginController.chekUserData(request)
-#     user = LoginController.checkCredentialse(email, password)
-
-#     if user == None: 
-#       return RESPONSE_SAMPLE.NOT_AUTHORIZED()
-
-#     token    = LoginController.generateToken(user)
-#     response = LoginController.generateResponse(user, token)
-
-#     return RESPONSE_SAMPLE.OK(response)
-
-#   except Exception as e:
-#     return RESPONSE_SAMPLE.BAD_REQUEST({ 'details': str(e) })
-  
+from client.authentication.Controller import LoginController
+from client.models                    import UserModel
 
 def LoginMiddleware(request: HTTP_REQUEST) -> HTTP_RESPONSE:
-  LoginController
+  try:
+    user: UserModel | None = LoginController.validateData(request)
+
+    if user != None:
+      return RESPONSE_SAMPLE.OK(
+        dat = {
+          'username': user.user_name,
+          'isAdmin' : user.role == "ADMIN",
+          'isAgent' : user.role == "AGENT",
+          'token'   : LoginController.generateToken(user)
+        }
+      )
+    
+    else:
+      return RESPONSE_SAMPLE.faillureBuild(
+        status_code = 401,
+        message     = "UNAUTHORIZED",
+        data        = { 'User': 'WRONG CREDENTIALS' }
+      )
+
+  except ValidationError as ve:
+    print(f"AN ERROR OCCURED {ve}")
+    return RESPONSE_SAMPLE.BAD_REQUEST({ str(ve.field): str(ve.message) })
+  except Exception as e:
+    print(f'LoginMiddleware {e}')
+    return RESPONSE_SAMPLE.BAD_REQUEST()
+  
