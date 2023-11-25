21a88201911fb2a7f9e6f4d8ea54105b98072bda
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-25 23:20:29 +0100

    code refactoring

--- server/client/authentication/Middleware/signup/signup_agent.py
+++ server/client/authentication/Middleware/signup/signup_agent.py
@@ -1,46 +1,24 @@
-from lib.HTTP     import HTTP_REQUEST, RESPONSE_SAMPLE
-from lib.token    import Token
+from lib.HTTP     import HTTP_REQUEST, HTTP_RESPONSE, RESPONSE_SAMPLE
+from lib.errors   import ValidationError
 
-from client.authentication.Controller.Signup     import SignUpController
+from client.authentication.Controller import SignupControllerAgent
 
-from client.Repository import RequestAgentRepository
+def SignupAgentMiddleware(request: HTTP_REQUEST) -> HTTP_RESPONSE:
+  try:    
+    if SignupControllerAgent(request):
+      return RESPONSE_SAMPLE.OK()
+    
+    else:
+      return RESPONSE_SAMPLE.faillureBuild(
+        status_code = 401,
+        message     = "UNAUTHORIZED",
+        data        = { 'User': 'WRONG CREDENTIALS' }
+      )
 
-def SignupAgentMiddleware(request: HTTP_REQUEST):
-  try:
-    agent_id = request.query.get('agent')
-    if agent_id == None:
-      return RESPONSE_SAMPLE.notFound()
-
-    #? CHECK IF THE REQUEST AGENT EXIST
-    isRequestAgentEntityExist = RequestAgentRepository.getRequestAgentById(agent_id, "PENDING")
-
-    if isRequestAgentEntityExist == None:
-      return RESPONSE_SAMPLE.badRequest({ 'request_agent': 'THIS REQUEST IS NO MORE VALID' })
-
-    #? -> Add role to request body
-    request.body['role']  = "AGENT"
-    request.body['email'] =  isRequestAgentEntityExist.email
-    (email, password, user_name, role) = SignUpController.validateUserData(request.body)
-
-    #! WE ARE NOT CHECKING FOR EMAIL BECAUSE WE ALREADY 
-    #! CHECKED IT BEFORE WE SENT TO REQUEST
-
-    #? CREATE USER
-    admin = isRequestAgentEntityExist.user_id
-    user = SignUpController.createUser(email, password, user_name, role, user = admin)
-
-    #? UPDATE REQUEST AGENT STATE
-    isRequestAgentEntityExist.state = 'ACCEPT'
-    isRequestAgentEntityExist.save()
-
-
-    #? CREATE TOKEN
-    token = Token.createToken({
-      'email'    : email,
-      'id' : user.id,
-    })
-
-    return RESPONSE_SAMPLE.successfullyCreadted({ 'token': token })
-
+  except ValidationError as ve:
+    print(f"AN ERROR OCCURED {ve}")
+    return RESPONSE_SAMPLE.BAD_REQUEST({ str(ve.field): str(ve.message) })
   except Exception as e:
-    return RESPONSE_SAMPLE.badRequest({ 'details': str(e) })
+    print(f'LoginMiddleware {e}')
+    return RESPONSE_SAMPLE.BAD_REQUEST()
+  
