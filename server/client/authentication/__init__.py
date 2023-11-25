73b85bb08528e75972976ce37aeff4776125d7f5
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-25 23:17:46 +0100

    authentication middleware

--- server/client/authentication/__init__.py
+++ server/client/authentication/__init__.py
@@ -1,3 +1,3 @@
-from .Middleware.currentUser import CurrentUserMiddleware
-from .Middleware.signup      import SignupMiddleware
+from .Middleware.signup_middleware      import SignupMiddleware
 from .Middleware.login_middleware       import LoginMiddleware
+from .Middleware.currentUser            import CurrentUserMiddleware
