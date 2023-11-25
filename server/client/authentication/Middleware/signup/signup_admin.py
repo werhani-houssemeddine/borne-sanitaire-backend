acdd390c09081f020f42762f87d8235dc02614cf
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-25 23:20:53 +0100

    code refcatoring

--- server/client/authentication/Middleware/signup/signup_admin.py
+++ server/client/authentication/Middleware/signup/signup_admin.py
@@ -1,43 +1,24 @@
-from lib.HTTP     import HTTP_REQUEST, RESPONSE_SAMPLE
-from lib.token    import Token
+from lib.HTTP     import HTTP_REQUEST, HTTP_RESPONSE, RESPONSE_SAMPLE
+from lib.errors   import ValidationError
 
-from client.Repository import UserRepository, DeviceRepository
-from client.authentication.Controller.Signup     import SignUpController
+from client.authentication.Controller import SignupControllerAdmin
 
-def SignupAdminMiddleware(request: HTTP_REQUEST):
-  try:
-    device_id = request.query.get('device')
+def SignupAdminMiddleware(request: HTTP_REQUEST) -> HTTP_RESPONSE:
+  try:    
+    if SignupControllerAdmin(request):
+      return RESPONSE_SAMPLE.OK()
     
-    #! This ckeck is already made in SignupMiddleware 
-    # if device_id == None:
-    #   return RESPONSE_SAMPLE.notFound()
+    else:
+      return RESPONSE_SAMPLE.faillureBuild(
+        status_code = 401,
+        message     = "UNAUTHORIZED",
+        data        = { 'User': 'WRONG CREDENTIALS' }
+      )
 
-    #? CHECK IF THE DEVICE EXIST
-    if DeviceRepository.getDeviceById(device_id) == None:
-      return RESPONSE_SAMPLE.badRequest({ 'device': 'THIS DEVICE ID IS NO MORE VALID' })
-
-    #? -> Add role to request body
-    request.body['role'] = "ADMIN"
-    (email, password, user_name, role) = SignUpController.validateUserData(request.body)
-
-    #? CHECK IF THE USER EXIST
-    if UserRepository.getUserByEmail(email) != None: # User exist
-      return RESPONSE_SAMPLE.badRequest({ 'email': 'THIS PROPERTY IS ALREADY USED' })
-
-    #? CREATE USER AND DEVICE
-    user   = SignUpController.createUser(email, password, user_name, role)
-
-    #! WE HAVE TO PASS USER TO DEVICE 
-    device = DeviceRepository.addDevice(device_id, user)
-
-    #? CREATE TOKEN
-    token = Token.createToken({
-      'email'    : email,
-      'id' : user.id,
-      'device_id': str(device.device_id)
-    })
-
-    return RESPONSE_SAMPLE.successfullyCreadted({ 'token': token })
-
+  except ValidationError as ve:
+    print(f"AN ERROR OCCURED {ve}")
+    return RESPONSE_SAMPLE.BAD_REQUEST({ str(ve.field): str(ve.message) })
   except Exception as e:
-    return RESPONSE_SAMPLE.badRequest({ 'details': str(e) })
+    print(f'LoginMiddleware {e}')
+    return RESPONSE_SAMPLE.BAD_REQUEST()
+  
