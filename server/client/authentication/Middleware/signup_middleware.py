89c8a17d9a52d48bade136e87050cdd02618270f
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-25 14:21:28 +0100

    signup middleware

---
+++ server/client/authentication/Middleware/signup_middleware.py
@@ -0,0 +1,17 @@
+from lib.HTTP import HTTP_REQUEST, RESPONSE_SAMPLE
+
+from .signup import SignupAdminMiddleware
+from .signup import SignupAgentMiddleware
+
+def SignupMiddleware(request: HTTP_REQUEST):
+  device_id = request.query.get('device')
+  agent_id  = request.query.get('agent')
+
+  if device_id == None and agent_id == None:
+    return RESPONSE_SAMPLE.notFound()
+
+  if device_id != None:
+    return SignupAdminMiddleware(request)
+
+  if agent_id != None:
+    return SignupAgentMiddleware(request)
