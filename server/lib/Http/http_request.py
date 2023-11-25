8fcfe57e1c1012c40012ac6ff75ec4957b638f71
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-25 12:10:33 +0100

    add type to body

--- server/lib/Http/http_request.py
+++ server/lib/Http/http_request.py
@@ -1,7 +1,7 @@
 # This class will create a HTTP_REQUEST we will work 
 # with this request object instead of the default http.Request 
 class HTTP_REQUEST:
-  def __init__(self, ip_address, headers, method, params, body, path, url, query, session):
+  def __init__(self, ip_address, headers, method, params, body: dict, path, url, query, session):
     self.headers = headers
     self.method  = method
     self.params  = params
