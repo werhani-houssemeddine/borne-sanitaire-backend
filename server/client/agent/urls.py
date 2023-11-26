1f4bf51b0c28477adf36e63772109b7d2407692e
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-26 15:32:47 +0100

    add param in the link

--- server/client/agent/urls.py
+++ server/client/agent/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
-  path('check-request-agent/', views.checkRequestedAgent),
+  path('check-request-agent/<str:id>', views.checkRequestAgent),
   path('reject-request/<str:id>/', views.rejectRequestAgent)
   # path('add/', ),
   # path('edit/', ),
