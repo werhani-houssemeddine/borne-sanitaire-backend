24e4b59fb6e33972bbaf97d1a4d7b47c57ca0f84
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-26 15:06:36 +0100

    accept request is in the signup endpoint

--- server/client/agent/Middleware/request_agent_middleware.py
+++ server/client/agent/Middleware/request_agent_middleware.py
@@ -2,9 +2,5 @@ from lib.HTTP import HTTP_REQUEST, HTTP_RESPONSE
 
 class RequestAgentMiddleware:
   @staticmethod
-  def acceptRequest(request: HTTP_REQUEST) -> HTTP_RESPONSE:
-    pass
-
-  @staticmethod
   def rejectRequest(request: HTTP_REQUEST) -> HTTP_RESPONSE:
     pass
