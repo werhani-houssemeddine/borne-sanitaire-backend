30a5123a571df2f37315004b34fb89261165ca1a
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-26 14:38:50 +0100

    implement getting all request , deleting one and getting one

--- server/client/admin/Middleware/request_agent_middleware.py
+++ server/client/admin/Middleware/request_agent_middleware.py
@@ -1,15 +1,19 @@
 from lib.errors import ServerError, ValidationError
 from lib.HTTP   import HTTP_REQUEST, HTTP_RESPONSE, RESPONSE_SAMPLE
 
-from client.admin.Controller import AddNewRequestAgentController
+from client.admin.Controller import AddNewRequestAgentController, RequestAgentController
 from client.admin.Service    import SendEmailToAgent 
+
+from client.models import RequestAgentModel
+
 class RequestAgentAdminMiddleware:
   @staticmethod
   def sendNewRequest(request: HTTP_REQUEST) -> HTTP_RESPONSE:
     try:
       request_agent = AddNewRequestAgentController(request)
       request_id    = request_agent.getRequestId()
-      print('TI TA7KI BERRASMI')
+      
+      #? Send Email to agent
       SendEmailToAgent.requestAgentEmail(
         username   = request.session['__currentUser__']['username'],
         request_id = request_id,
@@ -29,15 +33,46 @@ class RequestAgentAdminMiddleware:
   
   @staticmethod
   def getOneRequest(request: HTTP_REQUEST) -> HTTP_RESPONSE:
-    pass
+    try:
+      agent: RequestAgentModel = RequestAgentController.getSingleRequest(request)
+      return RESPONSE_SAMPLE.OK(data = agent)
+    
+    except ServerError as se:
+      return RESPONSE_SAMPLE.SERVER_ERROR()
+    
+    except ValidationError as ve:
+      if ve.field == "UNATHORIZED": return RESPONSE_SAMPLE.NOT_AUTHORIZED()
+      return RESPONSE_SAMPLE.BAD_REQUEST({ str(ve.field): str(ve.message) })
+    
+    except Exception as e:
+      return RESPONSE_SAMPLE.BAD_REQUEST()
+
 
   @staticmethod
   def getAllRequest(request: HTTP_REQUEST) -> HTTP_RESPONSE:
-    pass
+    try:
+      listOfRequestAgent = RequestAgentController.getAllRequestAgent(request)
+      return RESPONSE_SAMPLE.OK( data = listOfRequestAgent)
+    
+    except Exception:
+      return RESPONSE_SAMPLE.BAD_REQUEST()
 
   @staticmethod
   def deleteRequest(request: HTTP_REQUEST) -> HTTP_RESPONSE:
-    pass
+    try:
+      result: bool = RequestAgentController.deleteRequestAgent(request)
+      return RESPONSE_SAMPLE.OK(data = { 'delete': result })
+    
+    except ServerError as se:
+      return RESPONSE_SAMPLE.SERVER_ERROR()
+    
+    except ValidationError as ve:
+      if ve.field == "UNATHORIZED": return RESPONSE_SAMPLE.NOT_AUTHORIZED()
+      return RESPONSE_SAMPLE.BAD_REQUEST({ str(ve.field): str(ve.message) })
+    
+    except Exception as e:
+      print(e)
+      return RESPONSE_SAMPLE.BAD_REQUEST()
 
   #! For future version may the admin set the time for request to be in pending state default time is 7days
   # @staticmethod
