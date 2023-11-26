fa8ed6318dfc36ba95105eed99c57d427f271105
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-26 15:18:43 +0100

    request agent controller

---
+++ server/client/agent/Controller/request_agent_controller.py
@@ -0,0 +1,26 @@
+import uuid
+
+from lib.errors import ValidationError
+from lib.HTTP   import HTTP_REQUEST
+
+from client.Repository import RequestAgentRepository
+
+def validateUUID(uuid_str):
+  try:
+    return uuid.UUID(uuid_str)
+  except ValueError:
+    raise ValidationError('id', 'INVALID FORMAT')
+
+class RequestAgentController:
+  @staticmethod
+  def reject(request: HTTP_REQUEST) -> bool:
+    try:
+      request_id = validateUUID(request.params.get('id'))
+
+      if request_id is not None:
+        return RequestAgentRepository.updateRequestAgentState(request_id, 'REJECT')
+
+      raise ValidationError('REQUEST', 'REQUIRED REQUEST ID')
+      
+    except ValidationError: raise
+    except Exception: raise
