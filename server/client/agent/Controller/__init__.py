d4b32f6fe41ec675b1b194b752bd30edd9d788aa
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-26 15:19:16 +0100

    need to modify

--- server/client/agent/Controller/__init__.py
+++ server/client/agent/Controller/__init__.py
@@ -1,26 +1,28 @@
-from lib.HTTP.http_request     import HTTP_REQUEST
-from .bussinessLayer           import validateAgentData
+from .request_agent_controller import RequestAgentController
+
+# from lib.HTTP.http_request     import HTTP_REQUEST
+# from .bussinessLayer           import validateAgentData
     
-from client.Repository import UserRepository, RequestAgentRepository
+# from client.Repository import UserRepository, RequestAgentRepository
 
-class AgentController:
-  #? checkEmail function will validate the email than will check
-  #? check if the email is used by other user
-  #? or a requestAgent used it 
-  #? the method return the email if the email is used otherwise
-  #? it return None and raise an error if it's not valid
-  @staticmethod
-  def checkEmail(request: HTTP_REQUEST):
-    try:
-      email = validateAgentData(request.body["email"])
-      isEmailUsed = UserRepository.getUserByEmail(email)
-      isEmailUsedToRequestAgent = RequestAgentRepository.getRequestAgentByEmail(email)
+# class AgentController:
+#   #? checkEmail function will validate the email than will check
+#   #? check if the email is used by other user
+#   #? or a requestAgent used it 
+#   #? the method return the email if the email is used otherwise
+#   #? it return None and raise an error if it's not valid
+#   @staticmethod
+#   def checkEmail(request: HTTP_REQUEST):
+#     try:
+#       email = validateAgentData(request.body["email"])
+#       isEmailUsed = UserRepository.getUserByEmail(email)
+#       isEmailUsedToRequestAgent = RequestAgentRepository.getRequestAgentByEmail(email)
       
-      checkingEmailResult = isEmailUsed or isEmailUsedToRequestAgent
+#       checkingEmailResult = isEmailUsed or isEmailUsedToRequestAgent
 
-      #? If the email is used return NULL
-      #? otherwise return the email
-      return email if checkingEmailResult == None else None
+#       #? If the email is used return NULL
+#       #? otherwise return the email
+#       return email if checkingEmailResult == None else None
 
-    except Exception as e:
-      raise
+#     except Exception as e:
+#       raise
