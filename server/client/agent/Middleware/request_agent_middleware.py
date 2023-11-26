2a929944bcef1743c7937732f83001045e17d5a1
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-26 15:17:36 +0100

    reject request agent middleware

--- server/client/agent/Middleware/request_agent_middleware.py
+++ server/client/agent/Middleware/request_agent_middleware.py
@@ -1,6 +1,18 @@
-from lib.HTTP import HTTP_REQUEST, HTTP_RESPONSE
+from lib.errors import ServerError, ValidationError
+from lib.HTTP   import HTTP_REQUEST, HTTP_RESPONSE, RESPONSE_SAMPLE
+
+from agent.Controller import RequestAgentController
 
 class RequestAgentMiddleware:
   @staticmethod
   def rejectRequest(request: HTTP_REQUEST) -> HTTP_RESPONSE:
-    pass
+    try:
+      result: bool = RequestAgentController.reject(request)
+      return RESPONSE_SAMPLE.OK(data = { 'delete': result })
+        
+    except ValidationError as ve:
+      return RESPONSE_SAMPLE.BAD_REQUEST({ str(ve.field): str(ve.message) })
+    
+    except Exception as e:
+      print(e)
+      return RESPONSE_SAMPLE.BAD_REQUEST()
