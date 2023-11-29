6219125d7ed84b8381f63723238c93ddd110595a
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-29 22:31:35 +0100

    implement current function

--- server/client/current_user/views.py
+++ server/client/current_user/views.py
@@ -1,8 +1,15 @@
 from rest_framework.decorators import api_view
 
+from lib.make_request import makeRequest
+from lib.utils        import Authenticate
+
+from client.current_user.Middleware import UserMiddleware
+
 @api_view(['GET'])
+@Authenticate
 def current(request):
-  pass
+  return makeRequest(request = request, middleware = UserMiddleware.getCurrentUser)
+  
 
 @api_view(['PUT'])
 def update(request):
