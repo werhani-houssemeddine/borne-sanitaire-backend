25475ff042897625dc1860af70de3cf53564a03e
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-29 22:31:54 +0100

    add type to headers

--- server/lib/Http/http_request.py
+++ server/lib/Http/http_request.py
@@ -1,7 +1,9 @@
+from .headers import REQUEST_HEADERS
+
 # This class will create a HTTP_REQUEST we will work 
 # with this request object instead of the default http.Request 
 class HTTP_REQUEST:
-  def __init__(self, ip_address, headers, method, params, body: dict, path, url, query, session):
+  def __init__(self, ip_address, headers: REQUEST_HEADERS, method, params, body: dict, path, url, query, session):
     self.headers = headers
     self.method  = method
     self.params  = params
