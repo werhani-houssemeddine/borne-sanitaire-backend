9e8ff06b23ce3b5ea7e49ea0fb3c6936d75870b2
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-29 22:29:30 +0100

    add device endpoint

--- server/client/urls.py
+++ server/client/urls.py
@@ -4,14 +4,16 @@ from . import views
 AGENT_ENDPOINT  = include('client.agent.urls')
 ADMIN_ENDPOINT  = include('client.admin.urls')
 CLIENT_ENDPOINT = include('client.current_user.urls')
+DEVICE_ENDPOINT = include('client.device.urls')
 
 urlpatterns = [ 
   path('signup/', views.signup),
   path('login/', views.login),
-  path('current-user/', views.currentUser),
+  # path('current-user/', views.currentUser),
 
   path('admin/', ADMIN_ENDPOINT),
   path('agent/', AGENT_ENDPOINT),
+  path('device/', DEVICE_ENDPOINT),
 
-  path('/', CLIENT_ENDPOINT)
+  path('', CLIENT_ENDPOINT)
 ]
