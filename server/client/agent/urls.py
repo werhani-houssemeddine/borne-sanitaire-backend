73c22498dc439fdd62a33d23183b571a3293f6f4
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-30 21:07:51 +0100

    change endpoint format

--- server/client/agent/urls.py
+++ server/client/agent/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
-  path('check-request-agent/<str:id>', views.checkRequestAgent),
+  path('check-request-agent/', views.checkRequestAgent),
   path('reject-request/<str:id>/', views.rejectRequestAgent),
   path('active/', views.getActiveAgents),
   path('archeived/', views.getArcheivedAgents),
   path('pending/', views.gentPendingAgents),
   path('all/', views.getAllAgents),
-  path('<str:id>', views.getOneAgentData)
+  path('one/<int:id>/', views.getOneAgentData)
   # path('add/', ),
   # path('edit/', ),
   # path('archieve/', ),
