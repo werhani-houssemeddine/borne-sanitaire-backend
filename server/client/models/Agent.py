d3034e18feae6bcc4eeb87fe7a947ee6241737af
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-11-30 21:23:26 +0100

    add type

--- server/client/models/Agent.py
+++ server/client/models/Agent.py
@@ -1,15 +1,16 @@
 from django.db import models
 
+from .User import User
 class Agent(models.Model):
 
   id = models.AutoField( primary_key = True )
   suspend = models.BooleanField(default = False)
   
   #? User Id will reference to the admin
-  user_id = models.ForeignKey('User' , on_delete= models.CASCADE, related_name = "agent_user_id")
+  user_id: User = models.ForeignKey('User' , on_delete= models.CASCADE, related_name = "agent_user_id")
 
   #? Agent Id will reference to the agent from user table 
-  agent_id = models.ForeignKey('User', on_delete= models.CASCADE, related_name = "agent_agent_id")
+  agent_id: User = models.ForeignKey('User', on_delete= models.CASCADE, related_name = "agent_agent_id")
 
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True) 
