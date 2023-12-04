a03ac84b9f7cdc1c477723b5fb4f70f88117a98b
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-12-04 23:09:07 +0100

    add new model to save pictures

--- server/client/models/__init__.py
+++ server/client/models/__init__.py
@@ -1,3 +1,4 @@
 from .RequestAgent import RequestAgent as RequestAgentModel
 from .User         import User as UserModel
 from .Agent        import Agent as AgentModel
+from .UserPicture  import UserPicture as UserPictureModel
