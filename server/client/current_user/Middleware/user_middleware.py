93978c2563c6cba4eee34836de21df68ce7bb49d
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-12-06 22:44:57 +0100

    add check phone number endpoint

--- server/client/current_user/Middleware/user_middleware.py
+++ server/client/current_user/Middleware/user_middleware.py
@@ -1,8 +1,10 @@
 from lib.HTTP import HTTP_REQUEST, RESPONSE_SAMPLE
 
-from client.current_user.Controller import UserController
+from client.current_user.Controller import UserController, ValidateUserData
+from .update_user_middleware        import UpdateUserMiddleware
 
-from .update_user_middleware import UpdateUserMiddleware
+from client.Repository import UserRepository 
+
 class UserMiddleware(UpdateUserMiddleware):
   @staticmethod
   def getCurrentUser(request: HTTP_REQUEST):
@@ -11,3 +13,18 @@ class UserMiddleware(UpdateUserMiddleware):
       data = response.toJSON()
     )
   
+  #? This endpoint returns true if the phone number does not exist 
+  #? this will be used to verify if the client can use this phone number
+
+  @staticmethod
+  def checkPhoneNumber(request: HTTP_REQUEST):
+    phone_number = request.params.get('phone')
+    if ValidateUserData.validatePhoneNumber(phone_number):
+        if UserRepository.getUserByPhoneNumber(phone_number) == None:
+          return RESPONSE_SAMPLE.OK()
+        else:
+          return RESPONSE_SAMPLE.NOT_FOUND()
+
+    else:
+      return RESPONSE_SAMPLE.BAD_REQUEST({'phone_number': 'Invalid format'})
+      
