52d128832872585928b9fa7d6ac9581c5d1e8800
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-12-06 07:22:51 +0100

    add media directory

--- server/client/current_user/update/urls.py
+++ server/client/current_user/update/urls.py
@@ -4,9 +4,15 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [ 
   path('password/', views.updatePassword),
   path('profile-photo/', views.updateProfilePhoto),
   path('phone-number/', views.updatePhoneNumber),
   path('username/', views.updateUsername),
 ]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- server/server/settings.py
+++ server/server/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import environ 
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-r*z5+inh-tgpn!cc@vz()5bd3vju40$&=v3ulcgy=!559w!_ky
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -156,3 +157,6 @@ BCRYPT_ROUNDS = 12
 
 # CORS ALLOW ACCESS TO ALL DOMAIN
 CORS_ORIGIN_ALLOW_ALL = True
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
