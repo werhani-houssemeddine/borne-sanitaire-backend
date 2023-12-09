df01691b2a29ffabf6ec56f44318f6d810ed3e57
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-12-09 19:43:51 +0100

    UserPictureRepository

--- server/client/Repository/__init__.py
+++ server/client/Repository/__init__.py
@@ -2,3 +2,4 @@ from .RequestAgent_repository import RequestAgentRepository
 from .Device_repository       import DeviceRepository
 from .Agent_repository        import AgentRepository
 from .User_repository         import UserRepository
+from .Picture_repository      import UserPictureRepository
