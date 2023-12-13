5c4e8819ce788f5c44458d97bf7eaca7f3534ef6
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2023-12-13 03:44:10 +0100

    urls

--- server/client/urls.py
+++ server/client/urls.py
@@ -6,9 +6,14 @@ ADMIN_ENDPOINT  = include('client.admin.urls')
 CLIENT_ENDPOINT = include('client.current_user.urls')
 DEVICE_ENDPOINT = include('client.device.urls')
 
+AUTHENTICATION_ENDPOINT = include('client.authentication.urls')
+
 urlpatterns = [ 
   path('signup/', views.signup),
   path('login/', views.login),
+
+  path('authenticate/', AUTHENTICATION_ENDPOINT),
+
   # path('current-user/', views.currentUser),
 
   path('admin/', ADMIN_ENDPOINT),
